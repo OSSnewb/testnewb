fc432157461beb417a1c312ef03ab732384f06f4
OSSnewb <100742423+OSSnewb@users.noreply.github.com>
2022-05-04 10:53:07 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data= True,
+  name='testnewb",
+  version='0.0.1',
+  description='oss-development class testnewb',
+  author='Ossnewb',
+  author_email='kik1232198@gmail.com',
+  url="https://github.com/OSSnewb/testnewb"
+  download_url="
+  install_requries=['pytest']
+  long_description='oss-description
+  long_description_content_type="text/markdown',
+  classfilters=[
+    "Programmong Laanguage :: python :: 3",
+    "operationg System :: OS Independent",
+  ],
+)
